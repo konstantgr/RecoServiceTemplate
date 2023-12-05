053a2352826004ba4ddbb74ea6834c060afcd204
konstantgr <konstantin.grotov@gmail.com>
2023-12-06 00:55:55 +0100

    Implemented models and Performed analysis
    - Implemented hyperparameters tuning for ALS model using Optuna, also tuned parameters manually for other models, for the best of them created separate online-model classes.
    - Implemented ANN for recommendations, the following model is `VectorBasedAnnModel`
    - Implemented recommendations using user's features
    - All models were implemented for `online` inference
    - Tuned models beat baseline

--- models/models_mapping.py
+++ models/models_mapping.py
@@ -8,3 +8,7 @@ class Config(BaseSettings):
 class ModelsMapping(Config):
     popular_model_path: str = "model_weights/popular_model.json"
     userknn_model_path: str = "model_weights/userknn_model.pkl"
+    als_model_path: str = "model_weights/ALS_4.pkl"
+    lfm_logistic_4_model_path: str = "model_weights/LightFM_logistic_4.pkl"
+    lfm_warp_4_model_path: str = "model_weights/LightFM_warp_4.pkl"
+    lfm_logistic_4_ann_model_path: str = "model_weights/LightFM_logistic_4_ANN.pkl"
--- models/online_models.py
+++ models/online_models.py
@@ -1,9 +1,14 @@
 import json
 import pickle
-
-from models.base_model import BaseOnlineModel
+from pathlib import Path
 from typing import List
 
+import numpy as np
+import pandas as pd
+from rectools import Columns
+from rectools.dataset.dataset import Dataset
+
+from models.base_model import BaseOnlineModel
 from models.models_mapping import ModelsMapping
 
 
@@ -40,6 +45,53 @@ class UserKnnModel(BaseOnlineModel):
         return reco
 
 
+class DatasetEmpoweredRecommendationModel(BaseOnlineModel):
+    def __init__(self, model_path: str,
+                 dataset_base_path: str = "data/") -> None:
+        self.dataset = self.prepare_dataset(dataset_base_path)
+
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
+
+    @staticmethod
+    def prepare_dataset(dataset_base_path: str) -> Dataset:
+        path = Path(dataset_base_path)
+
+        Columns.Datetime = 'last_watch_dt'
+        interactions = pd.read_csv(path / 'interactions.csv')
+
+        interactions[Columns.Datetime] = pd.to_datetime(
+            interactions[Columns.Datetime], format='%Y-%m-%d')
+        interactions[Columns.Weight] = np.where(
+            interactions['watched_pct'] > 10, 3, 1)
+        dataset = Dataset.construct(
+            interactions_df=interactions
+        )
+        return dataset
+
+    def get_reco(self, user_id: int, k_recs: int) -> List[int]:
+        reco = self.model.recommend(
+            users=[user_id],
+            dataset=self.dataset,
+            k=k_recs,
+            filter_viewed=True,
+        ).item_id.tolist()
+        return reco
+
+
+class VectorBasedAnnModel(BaseOnlineModel):
+    def __init__(self, model_path: str) -> None:
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
+
+    def get_reco(self, user_id: int, k_recs: int) -> List[int]:
+        if user_id in self.model.user_id_map.external_ids:
+            reco = self.model.get_item_list_for_user(user_id, top_n=k_recs).tolist()
+            return reco
+        else:
+            return list(range(k_recs))
+
+
 class AvailableModelsDict(dict):
     def get(self, key, default=None, exception=None):
         res = super().get(key, default)
@@ -53,6 +105,10 @@ AVAILABLE_MODELS = AvailableModelsDict(
     {
         "simple_model": DummyModel(),
         "popular_model": JsonModel(models_mapping.popular_model_path),
-        "knn_model": UserKnnModel(models_mapping.userknn_model_path)
+        "knn_model": UserKnnModel(models_mapping.userknn_model_path),
+        "als_model": DatasetEmpoweredRecommendationModel(models_mapping.als_model_path),
+        "lfm_log_model": DatasetEmpoweredRecommendationModel(models_mapping.lfm_logistic_4_model_path),
+        "lfm_warp_model": DatasetEmpoweredRecommendationModel(models_mapping.lfm_warp_4_model_path),
+        "lfm_log_ann_model": VectorBasedAnnModel(models_mapping.lfm_logistic_4_model_path)
     }
 )
